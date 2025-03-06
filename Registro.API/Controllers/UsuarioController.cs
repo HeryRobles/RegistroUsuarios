4da7cc109d553fcd03e9ea61b630baf6bf7a345f
Hery Robles <rahe316426@gmail.com>
2025-03-05 19:38:22 -0700

    DTOs agregados

---
+++ Registro.API/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Registro.BLL;
+//using Registro.Model.Entities;
+//using System.Threading.Tasks;
+//using System.Collections.Generic;
+
+//namespace Registro.API.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class UsuarioController : ControllerBase
+//    {
+//    }
+//}
---
+++ Registro.DTO/MenuRolDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro.DTO
+{
+    public class MenuRolDTO
+    {
+        public int IdMenuRol { get; set; }
+        public int IdMenu { get; set; }
+    }
+}
